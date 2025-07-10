2a2e00eaf08296dd7b1fdf4dface4fc363f41c65
“Dmytro <Kulish.dmytro@gmail.com>
2025-07-10 21:29:27 +0300

    trying to replace dataframe in example_gen

--- pipeline/components/bq_example_gen.py
+++ pipeline/components/bq_example_gen.py
@@ -2,15 +2,14 @@
 
 from typing import Optional, Dict, Any
 import apache_beam as beam
-import tensorflow as tf
-from google.cloud import bigquery
-import pandas as pd
 
 from tfx.components.example_gen.base_example_gen_executor import BaseExampleGenExecutor
 from tfx.components.example_gen.component import FileBasedExampleGen
 from tfx.components.example_gen import utils
 from tfx.dsl.components.base import executor_spec
 from tfx.types import standard_artifacts
+
+import tensorflow as tf
 
 
 @beam.ptransform_fn
@@ -19,28 +18,27 @@ from tfx.types import standard_artifacts
 def _BigQueryToExample(
     pipeline: beam.Pipeline, exec_properties: Dict[str, Any], split_pattern: str
 ) -> beam.pvalue.PCollection:
-    """Read from BigQuery and create tf.Examples."""
+    """Reads from BigQuery in a scalable way and creates tf.Examples."""
+    project_id = exec_properties["custom_config"]["project_id"]
+    query = exec_properties["custom_config"]["query"]
 
-    def query_to_dict():
-        """Query BigQuery and yield dictionaries."""
-        project_id = exec_properties["custom_config"]["project_id"]
-        query = exec_properties["custom_config"]["query"]
-
-        client = bigquery.Client(project=project_id)
-        df = client.query(query).to_dataframe()
-
-        for _, row in df.iterrows():
-            yield {
-                "user_id": str(row["cust_person_id"]),
-                "product_id": str(row["product_id"]),
-                "city": str(row["city"]),
-                "sell_val_nsp": float(row["sell_val_nsp"]),
-                "date_of_day": str(row["date_of_day"]),
-            }
+    def format_row(bq_row: Dict[str, Any]) -> Dict[str, Any]:
+        """Formats a BigQuery row into a dictionary with the correct feature names and types."""
+        return {
+            "user_id": str(bq_row["cust_person_id"]),
+            "product_id": str(bq_row["product_id"]),
+            "city": str(bq_row["city"]),
+            "sell_val_nsp": float(bq_row["sell_val_nsp"]),
+            "date_of_day": str(bq_row["date_of_day"]),
+        }
 
     return (
         pipeline
-        | "CreateData" >> beam.Create(list(query_to_dict()))
+        | "ReadFromBigQuery"
+        >> beam.io.ReadFromBigQuery(
+            query=query, use_standard_sql=True, project=project_id
+        )
+        | "FormatRow" >> beam.Map(format_row)
         | "ToTFExample" >> beam.Map(utils.dict_to_example)
     )
 
